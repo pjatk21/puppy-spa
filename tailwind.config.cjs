11dafa4d3e150da987202c5a1a70e65d4b39bfc8
Krystian Postek <krystian@postek.eu>
2022-08-28 14:34:31 +0200

    Format tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,11 @@ module.exports = {
   content: [
     'src/**/*.{tsx,jsx}',
     'index.html',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'media'
+  plugins: [require('flowbite/plugin')],
+  darkMode: 'media',
 }
